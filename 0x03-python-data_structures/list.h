f6297fc638e41e9dcec3b91b482f5169c6e2b817
enanerose <enanerose97@gmail.com>
2022-09-27 13:46:41 -0700

    13 is a paliandrome

--- 0x03-python-data_structures/list.h
+++ 0x03-python-data_structures/list.h
@@ -1,57 +1,27 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
 
-#define fi if
-#define __local __attribute__((weak))
-
-typedef unsigned int uint;
-
 /**
- * enum rets - yes no values
- * @yes: 1
- * @no: 0
- */
-enum rets
-{
-	no,
-	yes
-};
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+* Description: singly linked list node structure*/
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for project
- */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+
+    struct listint_s *next;
+
 } listint_t;
-
-/**
- * len - find size of a list
- * @ls: list
- * Return: len
- */
-__local uint len(listint_t *ls)
-{
-	uint i;
-
-	for (i = 0; ls; i++)
-		ls = ls->next;
-
-	return (i);
-}
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int is_palindrome(listint_t **head);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
