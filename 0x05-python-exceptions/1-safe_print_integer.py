e2011f9c285f275428d54d5d70f0e6c89d083504
enanerose <enanerose97@gmail.com>
2022-10-11 14:23:10 -0700

    python exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
+
 def safe_print_integer(value):
+    """Print value with {:d}.format() format specifier and handle any ValueError
+       exceptions.
+
+    """
     try:
         print("{:d}".format(value))
         return True
